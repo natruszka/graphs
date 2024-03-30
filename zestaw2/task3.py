41ce57f15d1d4a26634b593277d54303a87bb6a9
natruszka <ola.michalska2002@gmail.com>
2024-03-30 19:16:48 +0100

    task3 and gitignore

--- zestaw2/task3.py
+++ zestaw2/task3.py
@@ -2,4 +2,24 @@
 
 # Napisać program do znajdowania największej spójnej składowej na grafie.
 
+import task1and2 as t12
 import networkx as nx
+
+def DFS(graph: nx.Graph, node: int, visited: list, nodes_in_component):
+    if node not in visited:
+        visited.append(node)
+        nodes_in_component.append(node)
+        for neighbor in graph.neighbors(node):
+            nodes_in_component = DFS(graph, neighbor, visited, nodes_in_component) 
+    return nodes_in_component
+
+def init_DFS(graph: nx.Graph, visited: list):
+    visited = []
+    all_components = []
+    for node in graph.nodes():
+        component = []
+        all_components.append(DFS(graph, node, visited, component))
+    return all_components
+
+all_components = init_DFS(t12.create_graph_from_sequence([1, 3, 2, 3, 2, 4, 1]), [])
+print(all_components)
