c112b3db238c4fd3555dd5774f52c965a5561f3c
arrudy <95677198+arrudy@users.noreply.github.com>
2024-05-08 19:32:45 +0200

    final modifications

--- zestaw3/task3and4.py
+++ zestaw3/task3and4.py
@@ -1,49 +1,23 @@
 import random as rng
 import networkx as nx
-
-def generate_connected_graph(node_n, edge_n):
-    G = nx.Graph()
-    G.add_nodes_from(range(node_n))
-
-    while nx.number_connected_components(G) > 1:
-
-        
-
-        #degrees = [(node ,val) for (node, val) in G.degree()]
-        #remaining = map(sum, map(lambda x : x[1] == 0 or x[1] == 1  ,degrees))
-        #print(remaining)
-        #print(degrees)
-
-        node1, node2 = rng.sample(range(node_n),2)
-
-        if not G.has_edge(node1, node2):
-            G.add_edge(node1, node2,weight=rng.randint(0,10)+1)
-
-    # Add remaining edges
-    remaining_edges = edge_n - G.number_of_edges()
-    for _ in range(remaining_edges):
-        node1, node2 = rng.sample(range(node_n),2)
-        if not G.has_edge(node1, node2):
-            G.add_edge(node1, node2,weight=rng.randint(0,10)+1)
-
-    return G
+import prj3_zad1
+import prj3_zad2
+import numpy as np
 
 
-node_n = 10
-edge_n = 15
-graph = generate_connected_graph(node_n, edge_n)
+graph = prj3_zad1.RGraph()
+graph.randomize(vertice_count=10,edges_count=16)
+graph = graph.to_nxGraph()
+
 
 print("N of nodes:", graph.number_of_nodes())
 print("N of edges:", graph.number_of_edges())
 print("Is connected:", nx.is_connected(graph))
 
-#/\ to be removed
 
 
-
-
-#z3.zip                                             \/ to be substituted
-resultz3 = [[i[1]  for i in  sorted(list(nx.single_source_dijkstra(graph,node)[0].items()),key = lambda x : x[0]) ] for node in graph.nodes]
+#z3.zip
+resultz3 = np.array([[ i[1]  for i in  sorted(list(nx.single_source_dijkstra(graph,node)[0].items()),key = lambda x : x[0]) ] for node in graph.nodes])
 print(resultz3)
 
 #z3, but decompressed
--- zestaw3/z5.py
+++ zestaw3/z5.py
@@ -1,10 +1,14 @@
+import prj3_zad1
+import networkx as nx
+import matplotlib.pyplot as plt
+
 '''
 graph = [
   [0,0,5,1],
   [0,0,4,6],
   [5,4,0,10],
   [1,6,10,0]
-]'''
+]
 
 graph = [
   [0,0,5,1,7],
@@ -12,7 +16,16 @@ graph = [
   [5,4,0,10,0],
   [1,6,10,0,0],
   [7,2,0,0,0]
-]
+]'''
+
+
+graph = prj3_zad1.RGraph()
+graph.randomize(vertice_count=10,edges_count=16)
+nxgraph = graph.to_nxGraph()
+graph = nx.adjacency_matrix(nxgraph).toarray()
+
+print(graph)
+
 
 elem = range(len(graph))
 edges = []
@@ -48,9 +61,9 @@ while len(tree_set) != 1:
     tree_set.remove(connection[1])
     tree_set.append(connection[0].union(connection[1])) #i laczymy oba drzewa
   edges.pop(0)
-  print(edge_set)
-  print(tree_set)
-  print()
+  #print(edge_set)
+  #print(tree_set)
+  #print()
   
 print(edge_set)
 print(tree_set)
@@ -58,9 +71,35 @@ print(tree_set)
 
 
 
+result = nx.Graph()
+
+for (x, cost) in edge_set:
+  x = list(x)
+  result.add_edge(x[0], x[1], weight = cost)
+
+pos = nx.spring_layout(result, seed=7)
+
+plt.subplot(1,2,1)
+nx.draw_networkx_nodes(nxgraph, pos, node_size=300)
+nx.draw_networkx_labels(nxgraph, pos, font_size=10, font_family="sans-serif")
+nx.draw_networkx_edges(
+    nxgraph, pos, width=4, alpha=0.2, edge_color="b", style="dashed"
+)
+edge_labels = nx.get_edge_attributes(nxgraph, "weight")
+nx.draw_networkx_edge_labels(nxgraph, pos, edge_labels)
+
+
+plt.subplot(1,2,2)
+nx.draw_networkx_nodes(result, pos, node_size=300)
+nx.draw_networkx_labels(result, pos, font_size=10, font_family="sans-serif")
+nx.draw_networkx_edges(
+    result, pos, width=5, alpha=1.0, edge_color="r", style="solid"
+)
+edge_labels = nx.get_edge_attributes(result, "weight")
+nx.draw_networkx_edge_labels(result, pos, edge_labels)
 
 
 
 
-
+plt.show()
 
