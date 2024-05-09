5025703db335f9b155d70b4ccea3dff42769b457
arrudy <95677198+arrudy@users.noreply.github.com>
2024-05-09 11:52:45 +0200

    use our implementation of dijkstra

--- zestaw3/task3and4.py
+++ zestaw3/task3and4.py
@@ -5,9 +5,9 @@ import prj3_zad2
 import numpy as np
 
 
-graph = prj3_zad1.RGraph()
-graph.randomize(vertice_count=10,edges_count=16)
-graph = graph.to_nxGraph()
+ourgraph = prj3_zad1.RGraph()
+ourgraph.randomize(vertice_count=10,edges_count=16)
+graph = ourgraph.to_nxGraph()
 
 
 print("N of nodes:", graph.number_of_nodes())
@@ -19,20 +19,26 @@ print("Is connected:", nx.is_connected(graph))
 #z3.zip
 resultz3 = np.array([[ i[1]  for i in  sorted(list(nx.single_source_dijkstra(graph,node)[0].items()),key = lambda x : x[0]) ] for node in graph.nodes])
 print(resultz3)
+print()
+
 
 #z3, but decompressed
-'''for node in graph.nodes:
-    res = nx.single_source_dijkstra(graph,node)
-    print(list(res[0].items()))
-    print(sorted(list(res[0].items()),key = lambda x : x[0]))
-    print(  [i[1]  for i in  sorted(list(res[0].items()),key = lambda x : x[0]) ]   )
-'''
+resultz3 = []
+for node in graph.nodes:
+    res = prj3_zad2.Dijkstra(ourgraph,node)
+    paths = prj3_zad2.Undijkstrify(res, node)
+    lengths = prj3_zad2.Lengthify(paths, node , ourgraph.edges)
+    resultz3.append(lengths)
+    
+resultz3 = np.array(resultz3)
+print(resultz3)
+
 
 #z4.zip
 graph_center = min([(i,sum(distances)) for i, distances in enumerate(resultz3)],key=lambda x : x[1])
-print(graph_center)
+print('Centrum grafu:',graph_center)
 graph_minmax = min([(i,max(distances)) for i, distances in enumerate(resultz3)],key=lambda x : x[1])
-print(graph_minmax)
+print('Minmax grafu:',graph_minmax)
 
 
 
